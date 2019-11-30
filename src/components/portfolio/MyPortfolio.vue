f018a37f2e5f703eb2eff7da093cc01c6c602296
Mohamed <mohamed.pe20188@gmail.com>
2019-11-30 14:07:19 +0200

    Potfolio Section

--- src/components/portfolio/MyPortfolio.vue
+++ src/components/portfolio/MyPortfolio.vue
@@ -1,13 +1,56 @@
 <template>
-  <div class="my-prtfolio">
-    <div class="row">
-      
+  <div class="my-portfolio">
+    <div class="project" :id="project.item" v-for="project in projects" :key="project.title">
+      <img :src="project.img" alt />
+      <div class="content">
+        <h4>{{ project.title }}</h4>
+        <div class="buttons">
+          <button class="info-btn">More Details</button>
+          <button class="live-btn">Live Preview</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MyPortfolio"
+  name: "MyPortfolio",
+  data() {
+    return {
+      projects: [
+        {
+          item: "one",
+          img: require("@/assets/portfolio/personal.png"),
+          title: "Personal Portfolio",
+          url: "#"
+        },
+        {
+          item: "two",
+          img: require("@/assets/portfolio/CV.png"),
+          title: "Personal Portfolio 2",
+          url: "#"
+        },
+        {
+          item: "three",
+          img: require("@/assets/portfolio/motoCar.jpg"),
+          title: "Personal Portfolio 3",
+          url: "#"
+        },
+        {
+          item: "four",
+          img: require("@/assets/portfolio/wolfCapital.jpg"),
+          title: "Personal Portfolio 4",
+          url: "#"
+        },
+        {
+          item: "five",
+          img: require("@/assets/portfolio/personal.png"),
+          title: "Personal Portfolio 5",
+          url: "#"
+        }
+      ]
+    };
+  }
 };
 </script>
